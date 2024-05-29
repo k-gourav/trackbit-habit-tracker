4922ba02353efc103760eb9aeb5bd7fb9abeedcd
k-gourav <code2change.gourav@gmail.com>
2024-05-29 18:17:03 +0530

    Update body component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,15 @@
-const Footer = ({ handleList, list }) => {
+import { useState } from "react"
+
+const Footer = ({ handleList, list}) => {
+    const [inputValue, setInputValue] = useState('')
     const addItem = (event) => {
-        handleList([event.target.value])
-        console.log([event.target.value])
+        handleList([...list, inputValue])
+        setInputValue('')
 
     }
     return (
         <div className="footer-section">
-            <input type="text" name="task-text" id="" />
+            <input value={inputValue} type="text" name="task-text" id="" onChange={(e) => setInputValue(e.target.value)} />
             <button onClick={addItem}>Enter</button>
         </div>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,6 @@ import notif from '../assets/notification-bell.png'
 import '../App.css'
 
 const Header = () => (
-    // <header>
-    //     <div className="top-section">
-    //         <div className="logo-container">
-    //             <img className='logo' src={logo} alt="logo-image" />
-    //         </div>
-    //         <div className="nav-items">
-    //             <ul>
-    //                 <li><a href="">Home</a></li>
-    //                 <li><a href="">Create</a></li>
-    //                 <li><a href="">Explore</a></li>
-    //             </ul>
-    //         </div>
-    //     </div>
-    // </header>
     <header>
             <div className="nav-items">
                 <h1>Hello, <span>Kumar👋</span></h1>
