fdde5cecc64a49947cecf8a2f01a4b84b0523999
Kumar Gourav <78784617+k-gourav@users.noreply.github.com>
2024-05-29 06:48:32 +0000

    Changes updated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,39 +1,47 @@
 import '../App.css'
 import calendar from '../assets/calendar.png'
 import goal from '../assets/target.png'
-
-const TaskList = ({description}) => (
+import Footer from '../components/Footer'
+import { useState } from 'react'
+const TaskList = ({ description }) => {
+    console.log(description)
+    return (
     <div className="task-list">
         <div className="task-descript">
-        <img className='task-logo' src={goal} alt="task-image" />
-        <p className="task-descript">{description}</p>
+            <img className='task-logo' src={goal} alt="task-image" />
+            <p className="task-descript">{description}</p>
         </div>
         {/* <div className='checkbox-custom'><input type="checkbox" name="task-checker"/></div> */}
         <div class="checkbox-wrapper-13">
-        <input id="c1-13" type="checkbox" />
-       </div>
-       </div>
-)
-
-const Body = () => (
-    <main className='main-container'>
-    <div className='day-status'>
-        <h2>Today's Goals</h2>
-        <img className='calendar-icon' src={calendar} alt="calendar-image" />
+            <input id="c1-13" type="checkbox" />
+        </div>
     </div>
-    
-    <div className="task-container">
-        <TaskList key="1" description="Complete CSS" imgLink={goal} />
-        <TaskList />
-        <TaskList />
-        <TaskList />
-        <TaskList />
-        <TaskList />
-        <TaskList />
-
-    </div>
-    
-    </main>
 )
+}
+
+const Body = () => {
+
+
+    const [list, setList] = useState([])
+    console.log(list)
+    return (<>
+        <main className='main-container'>
+            <div className='day-status'>
+                <h2>Today's Goals</h2>
+                <img className='calendar-icon' src={calendar} alt="calendar-image" />
+            </div>
+
+            <div className="task-container">
+                { list.map((element) => <TaskList key="1" description={element} imgLink={goal}  /> ) }
+
+
+            </div>
+        </main>
+        <Footer handleList={setList} list={list} />
+
+    </>
+    )
+}
+
 
 export default Body;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
-const Footer = () => (
-    <div className="footer-section">
-        <input type="text" name="task-text" id="" />
-        <button type="submit">Enter</button>
-    </div>
-)
+const Footer = ({ handleList, list }) => {
+    const addItem = (event) => {
+        handleList([event.target.value])
+        console.log([event.target.value])
 
+    }
+    return (
+        <div className="footer-section">
+            <input type="text" name="task-text" id="" />
+            <button onClick={addItem}>Enter</button>
+        </div>
+    )
+}
 export default Footer;
