acf59ae91c9963a8a2423faebb4dcf224d9296d2
k-gourav <code2change.gourav@gmail.com>
2024-05-30 18:37:55 +0530

    Added event handler for keydown

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,31 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const Footer = ({ handleList, list}) => {
-    const [inputValue, setInputValue] = useState('')
-    const addItem = (event) => {
-        handleList([...list, inputValue])
-        setInputValue('')
-
-    }
+const Footer = ({ handleList, list }) => {
+    const [inputValue, setInputValue] = useState('');
+    const addItem = () => {
+        if (inputValue.trim()) { 
+            handleList([...list, inputValue]);
+            setInputValue('');
+        }
+    };
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault(); 
+            addItem();
+        }
+    };
     return (
         <div className="footer-section">
-            <input value={inputValue} type="text" name="task-text" id="" onChange={(e) => setInputValue(e.target.value)} />
+            <input
+                value={inputValue}
+                type="text"
+                name="task-text"
+                onChange={(e) => setInputValue(e.target.value)}
+                onKeyDown={handleKeyDown}
+            />
             <button onClick={addItem}>Enter</button>
         </div>
-    )
-}
+    );
+};
+
 export default Footer;
